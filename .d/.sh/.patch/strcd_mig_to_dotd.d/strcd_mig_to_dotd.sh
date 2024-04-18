7cab6902cca080a6470d34b6b8d97f8d08c73ba3
[alt]legioner9 <legioner9@inbox.ru>
2024-04-18 21:39:05 +0700

    <> ##

--- .d/.sh/.patch/strcd_mig_to_dotd.d/strcd_mig_to_dotd.sh
+++ .d/.sh/.patch/strcd_mig_to_dotd.d/strcd_mig_to_dotd.sh
@@ -23,37 +23,33 @@ fi
 # _s2ad "/.st.rc.d" "/.d/.rc.d/.st.rc.d" ".st.sh.d.res"
 #*
 
-#* do /.st.rc.d
+#* do /.st.rc.d------------
 rm -r ${HOME}/.d/.rc.d/.st.rc.d
 cp -r ${HOME}/.st.rc.d/. ${HOME}/.d/.rc.d/.st.rc.d
-
+#! rm -r ${HOME}/~.st.rc.d~
 #! mv ${HOME}/.st.rc.d ${HOME}/~.st.rc.d~
-
 _s2ad "/.st.rc.d" "/.d/.rc.d/.st.rc.d" ${HOME}/.d/.rc.d/.st.rc.d
-#*
+#*-----------------
 
-#* tst ~.bashrc~
-# rm ${HOME}/~.bashrc~
-# mv ${HOME}/.bashrc ${HOME}/~.bashrc~
-# _s2f "/.st.rc.d" "/.d/.rc.d/.st.rc.d" ${HOME}/~.bashrc~
-#*
+#* do .bashrc----------------
+#! rm ${HOME}/~.bashrc~
+#! cp ${HOME}/.bashrc ${HOME}/~.bashrc~
+#! _s2f "/.st.rc.d" "/.d/.rc.d/.st.rc.d" ${HOME}/.bashrc
+#*-----------------
 
-#* do .bashrc
-#! mv ${HOME}/.bashrc ${HOME}/~.bashrc~
-
-#! _s2f "/.st.rc.d" "/.d/.rc.d/.st.rc.d" ${HOME}/~.bashrc~
-#*
-
-#* do .mane.rc
+#* do .mane.rc-----------------
 rm ${HOME}/.d/.rc.d/.mane.rc
 cp ${HOME}/.mane.rc ${HOME}/.d/.rc.d
 #! mv ${HOME}/.mane.rc ${HOME}/~.mane.rc~
+#*-----------------
 
-#* do .mane.rc
+#* do .st_rc_d.rc----------------
 rm ${HOME}/.d/.rc.d/.st_rc_d.rc
 cp ${HOME}/.st_rc_d.rc ${HOME}/.d/.rc.d
 #! mv ${HOME}/.st_rc_d.rc ${HOME}/~.st_rc_d.rc~
+#*-----------------
 
-
-_s2f /.mane.rc /.d/.rc.d/.mane.rc  ${HOME}/.d/.rc.d/.st_rc_d.rc/.st.lst
-_s2f /.st_rc_d.rc /.d/.rc.d/.st_rc_d.rc  ${HOME}/.d/.rc.d/.st_rc_d.rc/.st.lst
+#* do .st.lst
+_s2f /.mane.rc /.d/.rc.d/.mane.rc ${HOME}/.d/.rc.d/.st_rc_d.rc/.st.lst
+_s2f /.st_rc_d.rc /.d/.rc.d/.st_rc_d.rc ${HOME}/.d/.rc.d/.st_rc_d.rc/.st.lst
+#*-----------------
